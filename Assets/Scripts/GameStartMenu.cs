88b0ee1811487a9ab1cf51c7cd2a18571f7389f9
Rakshaan Khan <55557308+rakshaankhan@users.noreply.github.com>
2024-04-14 22:14:35 -0400

    UI Testing and FBX's.

---
+++ Assets/Scripts/GameStartMenu.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameStartMenu : MonoBehaviour
+{
+    [Header("UI Pages")]
+    public GameObject mainMenu;
+   
+
+    [Header("Main Menu Buttons")]
+    public Button startButton;
+   
+    public Button quitButton;
+
+    public List<Button> returnButtons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnableMainMenu();
+
+        //Hook events
+        startButton.onClick.AddListener(StartGame);
+       
+        quitButton.onClick.AddListener(QuitGame);
+
+        foreach (var item in returnButtons)
+        {
+            item.onClick.AddListener(EnableMainMenu);
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        HideAll();
+        SceneTransitionManager.singleton.GoToSceneAsync(1);
+    }
+
+    public void HideAll()
+    {
+        mainMenu.SetActive(false);
+       
+    }
+
+    public void EnableMainMenu()
+    {
+        mainMenu.SetActive(true);
+      
+    }
+  
+}
